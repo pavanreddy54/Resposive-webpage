4b86b3c742de10d1cc05f13d1c301114ccdf6a64
pavanreddy54 <sasipavan94@gmail.com>
2023-12-30 16:45:51 +0530

    window resizing selected query resloved

--- responsive.js
+++ responsive.js
@@ -20,17 +20,24 @@ function handleElementClick(element) {
     bubble.style.width = coords.width + 'px';
     bubble.style.height = coords.height + 'px';
     bubble.style.padding = '.rem';
+    bubble.style.outline= '1px solid black';
 
     updateFonts(element);
 }
-
+window.addEventListener('resize',()=>{
+    const selected=document.querySelector('.selected');
+    if(selected){
+        handleElementClick(selected);
+    }
+});
 function updateFonts(selectedElement) {
     for (const key in elements) {
         if (elements.hasOwnProperty(key)) {
             if (elements[key] !== selectedElement) {
+                elements[key].classList.remove('selected');
                 elements[key].style.color = 'black';
             }
         }
-    }
+    }selectedElement.classList.add('selected');
     selectedElement.style.color = 'white';
 }
